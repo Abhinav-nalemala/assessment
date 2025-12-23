6f9222802147cf16276fd50387902bb27fca555d
Abhinav-nalemala <161935373+Abhinav-nalemala@users.noreply.github.com>
2025-12-23 18:07:29 +0530

    Update demographic_data_analyzer.py

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -6,13 +6,13 @@ def calculate_demographic_data(print_data=True):
     df = pd.read_csv("adult.data.csv")
 
     # How many people of each race are represented in this dataset?
-    race_count = None
+    race_count = df['race'].value_counts()
 
     # What is the average age of men?
-    average_age_men = None
+    average_age_men = round(df[df['sex'] == 'Male']['age'].mean(), 1)
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    percentage_bachelors = round((df['education'] == 'Bachelors').mean() * 100, 1)
 
     # What percentage of people with advanced education make more than 50K?
     higher_education = None
